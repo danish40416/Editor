f5a39e4738acba51fc716f4110804d865e2089f9
Danish <haseebasif507@gmail.com>
2023-12-27 16:59:24 +0500

    test

---
+++ src/component/UI/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../NavBar/Navbar'
+import Main from '../Main/Main'
+
+export default function Layout() {
+  return (
+    <div>
+        <Navbar/>
+        <Main/>
+    </div>
+  )
+}
