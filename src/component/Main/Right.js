0ad08ecba5eca437c27126db6ae7db72be99fef9
Danish <haseebasif507@gmail.com>
2023-12-29 14:21:45 +0500

    create button

--- src/component/Main/Right.js
+++ src/component/Main/Right.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
+
 const Right = () => {
   const [currentView, setCurrentView] = useState('edit');
 
   const handleButtonClick = (view) => {
     setCurrentView(view);
   };
+
   return (
-    <div className="main-right">
-      <div className='mx-4 py-3 mb-0'>
-        <button className={`right-bar-btn  fw-bolder pe-5 ${currentView === 'edit' ? 'active' : ''}`}
-          onClick={() => handleButtonClick('edit')}  > Edit </button>
-        <button className={`right-bar-btn  fw-bolder ps-5 ${currentView === 'sheet' ? 'active' : ''}`}
+    <div className="main-right ">
+      <div className='py-3 right-top-btn'>
+        <button className={`right-bar-btn fw-bolder   ${currentView === 'edit' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('edit')}> Edit</button>
+        <button className={`right-bar-btn  fw-bolder  ${currentView === 'sheet' ? 'active' : ''}`}
           onClick={() => handleButtonClick('sheet')} >  Sheet </button>
       </div>
 
       <hr className='m-0' />
+
       {currentView === 'edit' && (
         <div>
           <div className='d-flex justify-content-center my-3'>
-            <button type="button" className="right-btn"> Nest</button>
+            <button type="button" className="right-btn"> Next</button>
           </div>
           <div className='ps-3'>
             <div>
@@ -26,6 +29,21 @@ const Right = () => {
               <p>WireFrame</p>
               <p>Canvas Background</p>
               <p>Background Color</p>
+            </div>
+          </div>
+        </div>
+      )}
+       {currentView === 'sheet' && (
+        <div>
+          <div className='d-flex justify-content-center my-3'>
+            <button type="button" className="right-btn"> Back</button>
+          </div>
+          <div className='ps-3'>
+            <div>
+              <p>Canvas Background</p>
+              <p>Background Color</p>
+              <p>Ruler</p>
+              <p>WireFrame</p>
             </div>
           </div>
         </div>
--- src/component/Main/Sidebar.js
+++ src/component/Main/Sidebar.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { PiImageSquareBold } from "react-icons/pi";
 import { TfiText } from "react-icons/tfi";
 
-export default function Left() {
+export default function sidebar() {
     return (
-        <div className='main-sidebar'>
+        <div className='main-sidebar positon-relative'>
             <div className='icon px-3 fs-4 pt-3'> <PiImageSquareBold /></div>
             <p className='icon px-2'>images</p>
             <div className=' icon px-3 fs-4 pt-3'> <TfiText /> </div>
             <p className='icon px-3'>Text</p>
+            <div className='positon-absolute '  >
+                <button className='sidebar-btn   mx-3  '> ?</button>
+            </div>
 
         </div>
     )
